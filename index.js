77d89ad89a0cc54d1032cb1b93046b3b6ac4981a
TravisChen1996 <travischen1996@gmail.com>
2022-05-16 18:18:36 +0800

    chapter: 0-2

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const Didact = {
+  render: (element, container) => {
+    let rootElement = document.createElement(element.type);
+    rootElement.children.forEach(child => {
+
+    })
+    container.appendChild(rootElement);
+  },
   createElement: (element, props, ...children) => {
     return {
       type: element,
@@ -8,18 +15,36 @@ const Didact = {
           if (typeof item === 'object') {
             return item;
           }
-          return {
-            type: 'TEXT_ELEMENT',
-            props: {
-              children: [],
-              nodeValue: item,
-            }
-          };
+          return createPureElement(item);
         })
       }
     };
   }
 };
+
+// ------
+
+function createPureElement(element) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      children: [],
+      nodeValue: item,
+    }
+  };
+}
+
+function createElement (element) {
+  let htmlNode = document.createElement(element.type);
+  element.props.forEach(attr => {
+    if (attr === 'children') {
+      return;
+    }
+    htmlNode.setAttribute('');
+  })
+}
+
+// ------
 
 const element = Didact.createElement(
   'h1', { title: 'tips' }, 'hello world'
