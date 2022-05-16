7554db539a1acd1d551c3f5fa0b41f15c379cce8
TravisChenn1996 <travischen1996@gmail.com>
2022-05-16 21:26:02 +0800

    chapter: 0-2

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const Didact = {
   render: (element, container) => {
-    let rootElement = document.createElement(element.type);
-    rootElement.children.forEach(child => {
-
-    })
-    container.appendChild(rootElement);
+    return createElement(element, container);
   },
   createElement: (element, props, ...children) => {
     return {
       type: element,
       props: {
         ...props,
-        children: children.map(function (item) {
+        children: children.map((item) => {
           if (typeof item === 'object') {
             return item;
           }
@@ -29,25 +25,43 @@ function createPureElement(element) {
     type: 'TEXT_ELEMENT',
     props: {
       children: [],
-      nodeValue: item,
+      nodeValue: element,
     }
   };
 }
 
-function createElement (element) {
-  let htmlNode = document.createElement(element.type);
-  element.props.forEach(attr => {
-    if (attr === 'children') {
-      return;
-    }
-    htmlNode.setAttribute('');
-  })
+function createElement(element, container) {
+  let htmlNode;
+  if (element.type === 'TEXT_ELEMENT') {
+    htmlNode = document.createTextNode(element.props.nodeValue);
+  } else {
+    htmlNode = document.createElement(element.type);
+    Object.keys(element.props).forEach(attr => {
+      if (attr === 'children') {
+        return;
+      }
+      htmlNode.setAttribute(attr, element.props[attr]);
+    });
+  }
+  element.props.children.forEach(item => createElement(item, htmlNode))
+  container.appendChild(htmlNode);
 }
 
 // ------
 
 const element = Didact.createElement(
-  'h1', { title: 'tips' }, 'hello world'
+  'h1',
+  {
+    title: 'tips',
+  },
+  'hello  h1',
+  Didact.createElement(
+    'h2',
+    null,
+    'hello h2'
+  )
 );
 
 console.log(element);
+
+Didact.render(element, document.getElementById('root'));
